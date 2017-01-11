b2827c4dfd7e45e884c4a6914fe1038dcc6b5c43
raphaelmonteiro <raphael.monteiro@ilegra.com>
2017-01-11 21:50:10 -0200

    Adding pocks

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,13 +1,12 @@
-var express = require('express');
-var app = express();
+v
+r express = require('express')
+var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+app.get('/', function(req, res){
+  res.send('Hello World')
+})
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
-});
+app.listen(3000, function(){
+  console.log('Running express application')
+})
